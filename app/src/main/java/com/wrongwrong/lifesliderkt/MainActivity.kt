b8f569b99b0018144d6a9a632e644e7571554822
wrongwrong <k163377@edu.tut.ac.jp>
2019-01-02 23:44:17 +0900

    ボタンでの上げ下げを追加

--- app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
@@ -2,11 +2,34 @@ package com.wrongwrong.lifesliderkt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
+    //シークバーをボタンで動かす用
+    fun minusStartBar(view: View) {
+        val temp = startSeekBar.progress - 1
+        startSeekBar.progress = min(104, max(0, temp))
+    }
+    fun plusStartBar(view: View) {
+        val temp = startSeekBar.progress + 1
+        startSeekBar.progress = min(104, max(0, temp))
+    }
+    fun minusEndBar(view: View) {
+        val temp = endSeekBar.progress - 1
+        endSeekBar.progress = min(105, max(1, temp))
+    }
+    fun plusEndBar(view: View) {
+        val temp = endSeekBar.progress + 1
+        endSeekBar.progress = min(105, max(1, temp))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }

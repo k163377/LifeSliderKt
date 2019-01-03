f5e864137cb42989e8b45c56a0545a616109cdf3
wrongwrong <k163377@edu.tut.ac.jp>
2019-01-03 18:57:25 +0900

    レート設定の初期値セットを追加

--- app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
@@ -44,5 +44,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //初期値を設定
+        setRate()
     }
 }

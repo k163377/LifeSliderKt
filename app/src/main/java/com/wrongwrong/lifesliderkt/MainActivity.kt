fbaf55306ab1a9b60e1c90df1812a4a3e22ed5ae
wrongwrong <k163377@edu.tut.ac.jp>
2019-01-03 16:55:01 +0900

    関数への分割とメソッド名変更

--- app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
@@ -8,23 +8,26 @@ import kotlin.math.max
 import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
-    //シークバーをボタンで動かす用
-    fun minusStartBar(view: View) {
-        val temp = startSeekBar.progress - 1
+    //シークバーのセット関数、moveで動作量制御
+    private fun setStartBar(move: Int = 0){
+        val temp = startSeekBar.progress + move
         startSeekBar.progress = min(104, max(0, temp))
+        if(endSeekBar.progress <= startSeekBar.progress){
+            endSeekBar.progress = startSeekBar.progress + 1
+        }
     }
-    fun plusStartBar(view: View) {
-        val temp = startSeekBar.progress + 1
-        startSeekBar.progress = min(104, max(0, temp))
-    }
-    fun minusEndBar(view: View) {
-        val temp = endSeekBar.progress - 1
+    private fun setEndBar(move: Int = 0){
+        val temp = endSeekBar.progress + move
         endSeekBar.progress = min(105, max(1, temp))
+        if(endSeekBar.progress <= startSeekBar.progress){
+            startSeekBar.progress = endSeekBar.progress - 1
+        }
     }
-    fun plusEndBar(view: View) {
-        val temp = endSeekBar.progress + 1
-        endSeekBar.progress = min(105, max(1, temp))
-    }
+    //シークバーをボタンで動かす用、タグを使って書き直せばもっとシンプルにできる
+    fun onClickMinusStartBar(view: View) { setStartBar(-1) }
+    fun onClickPlusStartBar(view: View) { setStartBar(1) }
+    fun onClickMinusEndBar(view: View) { setEndBar(-1) }
+    fun onClickPlusEndBar(view: View) { setEndBar(1) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

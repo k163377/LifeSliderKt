88e435c0634348082174ebbeba18223b85bd15e9
wrongwrong <k163377@edu.tut.ac.jp>
2019-01-03 19:09:41 +0900

    コメント追加

--- app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/lifesliderkt/MainActivity.kt
@@ -19,21 +19,25 @@ class MainActivity : AppCompatActivity() {
 
     //シークバーのセット関数、moveで動作量制御
     private fun setStartBar(move: Int = 0){
+        //スタートをセット
         val temp = startSeekBar.progress + move
         startSeekBar.progress = min(104, max(0, temp))
+        //エンドを調整
         if(endSeekBar.progress <= startSeekBar.progress){
             endSeekBar.progress = startSeekBar.progress + 1
         }
-
+        //テキスト系に反映
         setRate()
     }
     private fun setEndBar(move: Int = 0){
+        //スタートをセット
         val temp = endSeekBar.progress + move
         endSeekBar.progress = min(105, max(1, temp))
+        //エンドをセット
         if(endSeekBar.progress <= startSeekBar.progress){
             startSeekBar.progress = endSeekBar.progress - 1
         }
-
+        //テキスト系に反映
         setRate()
     }
     //シークバーをボタンで動かす用、タグを使って書き直せばもっとシンプルにできる
